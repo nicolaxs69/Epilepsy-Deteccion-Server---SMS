1775cd51605499e63a8dc667bb2f74ac0413f484
Nicolas Escobar Cruz <nicoescobar69@hotmail.com>
2017-11-28 00:59:00 -0500

    uodate number

--- sendMessage.js
+++ sendMessage.js
@@ -15,7 +15,7 @@ module.exports = function(telephone, text){
     client.messages.create({
         body: text,
         to: telephone,
-        from: "+15005550006" // cambiar este número
+        from: "+19093665653" // cambiar este número
     }, function(err, sms) {
         if(err){
           reject({error: err, ok: false});
