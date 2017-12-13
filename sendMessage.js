4ee473ec9fec6100bfc16f9a32e3bfc3a37f9654
Nicolas Escobar Cruz <nicoescobar69@hotmail.com>
2017-12-13 16:19:07 -0500

    cuenta de jhon Twilio

--- sendMessage.js
+++ sendMessage.js
@@ -1,6 +1,6 @@
 
-var authToken = "ca6dd7220c7f3fb012dfe48ff1baa3eb";
-var accountSid = 'AC4270069f8b44baaf951f8f465eaa687e  ';
+var authToken = "346a786fda8c475ba4d4afbfacb460bf";
+var accountSid = 'AC55685092d2d9bbdf359d84a548c70775';
 let client = require('twilio')(accountSid, authToken);
 let yo = "<instertar numero>"
 
@@ -15,7 +15,7 @@ module.exports = function(telephone, text){
     client.messages.create({
         body: text,
         to: telephone,
-        from: "+61428868990" // cambiar este número
+        from: "+15005550006" // cambiar este número
     }, function(err, sms) {
         if(err){
           reject({error: err, ok: false});
